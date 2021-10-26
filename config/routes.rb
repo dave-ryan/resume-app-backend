49c43006e2761dfe8a16e1a35165651023c41094
dave-a-ryan <daveryan145@gmail.com>
2021-10-26 11:44:29 -0500

    added students routes and controllers, requests http updated as well

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get "/students" => "students#index"
+  get "/students/:id" => "students#show"
+  post "/students" => "students#create"
+  patch "/students/:id" => "students#update"
+  delete "/students/:id" => "students#destroy"
   ### skills CRUD actions
   get "/skills" => "skills#index"
   get "/skills/:id" => "skills#show"
