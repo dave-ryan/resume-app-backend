ef6a3a114eeba2de07deb580e8ae4249643ba35b
Alexis Herrera <alexis36.herrera@gmail.com>
2021-10-26 11:05:20 -0500

    created CRUD actions for skills and index for projects

---
+++ app/controllers/skills_controller.rb
@@ -0,0 +1,40 @@
+class SkillsController < ApplicationController
+  def index
+    skills = Skill.all
+    render json: skills
+  end
+
+  def show
+    skill = Skill.find_by(id: params[:id])
+    render json: skill
+  end
+
+  def create
+    skill = Skill.create!(
+      skill_name: params[:skill_name],
+      student_id: current_user.id,
+    )
+    if skill.save
+      render json: post
+    else
+      render json: { error: skills.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def update
+    skill = Skill.find_by(id: params[:id])
+    skill.skill_name = params[:skill_name] || skill.skill_name
+
+    if skill.save
+      render json: skill
+    else
+      render json: { errors: skills.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def destroy
+    skill = Skill.find_by(id: params[:id])
+    skill.destroy
+    render json: { message: "skill succesfully destroyed!" }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   get "/students" => "students#index"
+  ### skills CRUD actions
+  get "/skills" => "skills#index"
+  get "/skills/:id" => "skills#show"
+  post "/skills" => "skills#create"
+  patch "/skills/:id" => "skills#upda†e"
+  delete "/skills/:id" => "skills#destroy"
+  ### projects index
+  get "/projects" => "projects#index"
 end
